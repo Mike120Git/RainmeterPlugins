a65df7d1c1e16c5ee7475c6e6e081a1f3643663a
Mike120Git <mike120@gmail.com>
2018-12-04 20:58:08 +0100

    initial commit

---
+++ C#/PluginMqttClient/PluginMqttClient.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using MQTTnet;
+using MQTTnet.Client;
+using Rainmeter;
+
+namespace PluginMqttClient
+{
+    class Measure : IDisposable
+    {
+        public string Topic;
+        public string Server;
+        private IMqttClient mqttClient;
+        public double Value = -1.0d;
+        private Rainmeter.API api;
+
+        public void Connect(Rainmeter.API api)
+        {
+            this.api = api;
+            try
+            {
+                Server = api.ReadString("Server", "")?.Trim();
+                Topic = api.ReadString("Topic", "")?.Trim();
+                api.Log(API.LogType.Notice, $"Connecting to MQTT; Server={Server}; Topic={Topic}");
+                var factory = new MqttFactory();
+                mqttClient = factory.CreateMqttClient();
+                var clientOptions = new MqttClientOptionsBuilder().WithTcpServer(Server).Build();
+                mqttClient.ConnectAsync(clientOptions).GetAwaiter().GetResult();
+                mqttClient.SubscribeAsync(Topic).GetAwaiter().GetResult();
+                mqttClient.ApplicationMessageReceived += MqttClient_ApplicationMessageReceived;
+            }
+            catch (Exception exception)
+            {
+                api.Log(API.LogType.Error, $"MQTT connection failed: {exception.Message}");
+            }
+        }
+
+        private void MqttClient_ApplicationMessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
+        {
+            var message = e.ApplicationMessage.ConvertPayloadToString();
+            api.Log(API.LogType.Notice, $"Message received; Message={message}; Topic={e.ApplicationMessage.Topic}");
+
+            try
+            {
+                Value = Convert.ToDouble(message, CultureInfo.InvariantCulture);                
+            }
+            catch (Exception exception)
+            {
+                api.Log(API.LogType.Error, exception.Message);
+                Value = -1.0d;
+            }
+
+        }
+
+        public void Dispose()
+        {
+            if (mqttClient != null)
+            {
+                mqttClient.Dispose();
+                mqttClient = null;
+            }
+        }
+
+        static public implicit operator Measure(IntPtr data)
+        {
+            return (Measure)GCHandle.FromIntPtr(data).Target;
+        }
+        public IntPtr buffer = IntPtr.Zero;
+    }
+
+    public class Plugin
+    {
+        [DllExport]
+        public static void Initialize(ref IntPtr data, IntPtr rm)
+        {
+            var measure = new Measure();
+            data = GCHandle.ToIntPtr(GCHandle.Alloc(measure));
+            var api = (Rainmeter.API)rm;
+            measure.Connect(api);
+        }
+
+        [DllExport]
+        public static void Finalize(IntPtr data)
+        {
+            Measure measure = (Measure)data;
+            measure.Dispose();
+
+            if (measure.buffer != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(measure.buffer);
+            }
+            GCHandle.FromIntPtr(data).Free();
+        }
+
+        [DllExport]
+        public static void Reload(IntPtr data, IntPtr rm, ref double maxValue)
+        {
+            Measure measure = (Measure)data;
+        }
+
+        [DllExport]
+        public static double Update(IntPtr data)
+        {
+            Measure measure = (Measure)data;
+            //api.Log(API.LogType.Notice, $"Update; Value={measure.Value}; Topic={measure.Topic}");
+            return measure.Value;
+        }
+
+        //[DllExport]
+        //public static IntPtr GetString(IntPtr data)
+        //{
+        //    Measure measure = (Measure)data;
+        //    if (measure.buffer != IntPtr.Zero)
+        //    {
+        //        Marshal.FreeHGlobal(measure.buffer);
+        //        measure.buffer = IntPtr.Zero;
+        //    }
+        //
+        //    measure.buffer = Marshal.StringToHGlobalUni("");
+        //
+        //    return measure.buffer;
+        //}
+
+        //[DllExport]
+        //public static void ExecuteBang(IntPtr data, [MarshalAs(UnmanagedType.LPWStr)]String args)
+        //{
+        //    Measure measure = (Measure)data;
+        //}
+
+        //[DllExport]
+        //public static IntPtr (IntPtr data, int argc,
+        //    [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPWStr, SizeParamIndex = 1)] string[] argv)
+        //{
+        //    Measure measure = (Measure)data;
+        //    if (measure.buffer != IntPtr.Zero)
+        //    {
+        //        Marshal.FreeHGlobal(measure.buffer);
+        //        measure.buffer = IntPtr.Zero;
+        //    }
+        //
+        //    measure.buffer = Marshal.StringToHGlobalUni("");
+        //
+        //    return measure.buffer;
+        //}
+    }
+}
+
---
+++ C#/PluginMySqlClient/PluginMySqlClient.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using MySql.Data.MySqlClient;
+using Rainmeter;
+
+namespace PluginMySqlClient
+{
+    class Measure : IDisposable
+    {
+        public string ConnectionString;
+        public string SqlQuery;
+        public int Interval;
+        public double Value = -1.0d;
+        private Rainmeter.API api;
+        System.Timers.Timer timer;
+
+        private void GetData()
+        {
+            //var connectionString = "Server=hellsgate.home.net;Port=3307;Database=meteo;Uid=meteo;Pwd=P@$$w0rd;";
+            using (var connection = new MySqlConnection(ConnectionString))
+            {
+                connection.Open();
+                using (var command = new MySqlCommand(SqlQuery, connection)) //"SELECT Value FROM smog_p25 ORDER BY Date DESC LIMIT 1;"
+                {
+                    var result = command.ExecuteScalar();
+                    try
+                    {
+                        Value = Convert.ToDouble(result, CultureInfo.InvariantCulture);
+                    }
+                    catch (Exception exception)
+                    {
+                        api.Log(API.LogType.Error, exception.Message);
+                        Value = -1.0d;
+                    }
+                    connection.Close();
+                }
+            }
+        }
+
+
+        public void Init(Rainmeter.API api)
+        {
+            this.api = api;
+            try
+            {
+                ConnectionString = api.ReadString("ConnectionString", "")?.Trim();
+                SqlQuery = api.ReadString("SqlQuery", "")?.Trim();
+                Interval = api.ReadInt("Interval", 5000);
+                api.Log(API.LogType.Notice, $"Connecting to MySql; ConnectionString={ConnectionString}; SqlQuery={SqlQuery}; Interval = {Interval}");
+                GetData();
+
+                timer = new System.Timers.Timer();
+                timer.Elapsed += Timer_Elapsed;
+                timer.Interval = Interval;
+                timer.Enabled = true;
+            }
+            catch (Exception exception)
+            {
+                api.Log(API.LogType.Error, $"MySql query failed: {exception.Message}");
+            }
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                GetData();
+            }
+            catch (Exception exception)
+            {
+                api.Log(API.LogType.Error, $"MySql query failed: {exception.Message}");
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        static public implicit operator Measure(IntPtr data)
+        {
+            return (Measure)GCHandle.FromIntPtr(data).Target;
+        }
+        public IntPtr buffer = IntPtr.Zero;
+    }
+
+    public class Plugin
+    {
+        [DllExport]
+        public static void Initialize(ref IntPtr data, IntPtr rm)
+        {
+            var measure = new Measure();
+            data = GCHandle.ToIntPtr(GCHandle.Alloc(measure));
+            var api = (Rainmeter.API)rm;
+            measure.Init(api);
+        }
+
+        [DllExport]
+        public static void Finalize(IntPtr data)
+        {
+            Measure measure = (Measure)data;
+            measure.Dispose();
+
+            if (measure.buffer != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(measure.buffer);
+            }
+            GCHandle.FromIntPtr(data).Free();
+        }
+
+        [DllExport]
+        public static void Reload(IntPtr data, IntPtr rm, ref double maxValue)
+        {
+            Measure measure = (Measure)data;
+        }
+
+        [DllExport]
+        public static double Update(IntPtr data)
+        {
+            Measure measure = (Measure)data;
+            //api.Log(API.LogType.Notice, $"Update; Value={measure.Value}; Topic={measure.Topic}");
+            return measure.Value;
+        }
+
+        //[DllExport]
+        //public static IntPtr GetString(IntPtr data)
+        //{
+        //    Measure measure = (Measure)data;
+        //    if (measure.buffer != IntPtr.Zero)
+        //    {
+        //        Marshal.FreeHGlobal(measure.buffer);
+        //        measure.buffer = IntPtr.Zero;
+        //    }
+        //
+        //    measure.buffer = Marshal.StringToHGlobalUni("");
+        //
+        //    return measure.buffer;
+        //}
+
+        //[DllExport]
+        //public static void ExecuteBang(IntPtr data, [MarshalAs(UnmanagedType.LPWStr)]String args)
+        //{
+        //    Measure measure = (Measure)data;
+        //}
+
+        //[DllExport]
+        //public static IntPtr (IntPtr data, int argc,
+        //    [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPWStr, SizeParamIndex = 1)] string[] argv)
+        //{
+        //    Measure measure = (Measure)data;
+        //    if (measure.buffer != IntPtr.Zero)
+        //    {
+        //        Marshal.FreeHGlobal(measure.buffer);
+        //        measure.buffer = IntPtr.Zero;
+        //    }
+        //
+        //    measure.buffer = Marshal.StringToHGlobalUni("");
+        //
+        //    return measure.buffer;
+        //}
+    }
+}
+
